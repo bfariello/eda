d5940126b7d380c0d0f8359017df064e1aebe6b5
bfari <bfari@10.132.11.131>
2023-08-31 10:16:05 -0300

    XZXZX Signed-off-by:bfari <bfari@10.132.11.131>

---
+++ practico1.h
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   practico1.h
+ * Author: bfari
+ *
+ * Created on 17 de agosto de 2023, 17:25
+ */
+
+#ifndef PRACTICO1_H
+#define PRACTICO1_H
+
+struct Lista;
+typedef Lista * ListaEnteros;
+
+
+
+bool isIncluded(ListaEnteros l1,  ListaEnteros l2);
+
+#endif /* PRACTICO1_H */
+
